1409257cf8c969bdff5d592f0cf7d5330acebf38
Wilson Widyadhana <wilsonwidyadhana681@gmail.com>
2024-10-18 13:45:01 +0800

    Resolve merge conflict

--- preprocessing/dct.py
+++ preprocessing/dct.py
@@ -1,8 +1,23 @@
 import cv2
 import numpy as np
 
+from typing import Optional
 
-def extract_dct_feature_from_video(video_path):
+
+def extract_dct_feature_from_video(video_path: str) -> Optional[np.ndarray]:
+    """
+    Extracts the Discrete Cosine Transform (DCT) from the grayscale of the image.
+
+    Args:
+        video_path (str): Path to the video as a string.
+
+    Returns:
+        Either `None` or a NumPy array representing the DCT.
+
+    Example Usage:
+        video_path = "../example-output/aapnvogymq.mp4" 
+        dct_features = extract_dct_feature_from_video(video_path)
+    """
     # Capture the video
     cap = cv2.VideoCapture(video_path)
 
@@ -33,17 +48,7 @@ def extract_dct_feature_from_video(video_path):
     # Release video capture object
     cap.release()
 
-    # Convert list of feature vectors to a numpy array
+    # Convert list of feature vectors to a NumPy array
     dct_features_array = np.array(dct_features)
 
     return dct_features_array
-
-
-"""
-# Example usage:
-video_path = "../example-output/aapnvogymq.mp4" 
-dct_features = extract_dct_feature_from_video(video_path)
-
-if dct_features is not None:
-    print(f"Extracted DCT feature shape: {dct_features.shape}")
-"""
