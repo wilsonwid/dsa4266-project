23c8793f5eb794076140a744e1969377a56074bd
Wilson Widyadhana <wilsonwidyadhana681@gmail.com>
2024-11-01 08:05:49 +0800

    Add rcnn code

--- models/rcnn/rcnn.py
+++ models/rcnn/rcnn.py
@@ -5,7 +5,10 @@ import torch.nn as nn
 from utils.types import NonlinearityEnum
 from utils.utils import select_nonlinearity
 
-class RecurrentConvolutionalBlock(nn.Module):
+class RecurrentConvolutionalLayer(nn.Module):
+    """
+    Class representing the recurrent convolutional layer.
+    """
     def __init__(
             self,
             input_dim: int,
@@ -76,8 +79,93 @@ class RecurrentConvolutionalBlock(nn.Module):
         return x
 
 class RecurrentConvolutionalNetwork(nn.Module):
-    def __init__(self):
+    def __init__(
+            self,
+            input_channels:  int,
+            num_recurent_layers: int,
+            num_kernels: int = 96,
+            kernel_size: int | tuple[int, int] = (3, 3),
+            stride: int | tuple[int, int] = (1, 1),
+            padding: int | tuple[int, int] = (1, 1),
+            dropout_prob: float = 0.25,
+            nonlinearity: NonlinearityEnum = NonlinearityEnum.RELU,
+            bias: bool = False,
+            steps: int = 5,
+            num_classes: int = 2
+        ):
         super().__init__()
+        self.input_channels = input_channels
+        self.num_recurrent_layers = num_recurent_layers
+        self.num_kernels = num_kernels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dropout_prob = dropout_prob
+        self.nonlinearity = nonlinearity
+        self.bias = bias
+        self.steps = steps
+        self.num_classes = num_classes
+
+        self.init_conv_layer = nn.Conv2d(
+            in_channels=self.input_channels,
+            out_channels=self.num_kernels,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            bias=self.bias
+        )
+
+        self.nonlinearity_layer = select_nonlinearity(self.nonlinearity)
+
+        self.batchnorm_layer = nn.BatchNorm2d(num_features=self.num_kernels)
+
+        self.pooling_layers = [nn.MaxPool2d(
+            kernel_size=self.kernel_size, 
+            stride=self.stride, 
+            padding=self.padding
+        ) for _ in range(self.num_recurrent_layers // 2)]
+
+        self.dropout_layers = [nn.Dropout(
+            p=self.dropout_prob,
+        ) for _ in range(self.num_recurrent_layers // 2)]
+
+        self.recurrent_convolutional_layers = [RecurrentConvolutionalLayer(
+            input_dim=self.num_kernels,
+            output_dim=self.num_kernels,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            bias=self.bias,
+            steps=self.steps,
+            nonlinearity=self.nonlinearity
+        ) for _ in range(self.num_recurrent_layers)]
+
+        self.fc = nn.Linear(
+            in_features=self.kernel_size,
+            out_features=self.num_classes,
+            bias=self.bias
+        )
+
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d()):
+                nn.init.kaiming_normal_(module.weight)
+            elif isinstance(module, nn.BatchNorm2d):
+                module.weight.data.fill_(1)
+                module.bias.data.zero_()
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        pass
+        x = self.init_conv_layer(x)
+        x = self.batchnorm_layer(self.nonlinearity_layer(x))
+        for i in range(0, self.num_recurrent_layers, 2):
+            x = self.pooling_layers[i//2](x)
+            x = self.recurrent_convolutional_layers[i](x)
+            if i != self.num_recurrent_layers - 1:
+                x = self.recurrent_convolutional_layers[i+1](x)
+            x = self.recurrent_convolutional_layers[i+1](x)
+            x = self.dropout_layers[i//2](x)
+        
+        x = nn.functional.max_pool2d(x, kernel_size=self.kernel_size)
+        x = x.view(-1, self.num_kernels)
+        x = nn.functional.dropout(x, p=self.dropout_prob)
+        x = self.fc(x)
+        return x
