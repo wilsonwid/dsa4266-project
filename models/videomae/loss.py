ee9adf7bc5666bbd1a5a961d6e229e968907421f
Valerie Tan <valerietanhx@gmail.com>
2024-11-13 13:59:36 +0800

    add videomae notebook

--- models/videomae/loss.py
+++ models/videomae/loss.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Note that videomae_loss.csv was manually created from the results automatically pushed to Huggingface:
+# https://huggingface.co/shylhy/videomae-large-finetuned-deepfake-subset/blob/2cbeca77c171af587f215a7d262ab6c47cbcea17/README.md
+
 df = pd.read_csv("models/videomae/results/videomae_loss.csv")
 
 plt.figure(figsize=(10, 6))
